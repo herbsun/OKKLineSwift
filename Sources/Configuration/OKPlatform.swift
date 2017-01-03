9ab27836c58b54e27defee86f985ffa58cf6bff1
huabei.sun@okcoin.com <huabei.sun@okcoin.com>
2017-01-03 16:54:04 +0800

    废除部分类

--- Sources/Configuration/OKPlatform.swift
+++ Sources/Configuration/OKPlatform.swift
@@ -232,3 +232,19 @@ public func OKPrint(_ object: @autoclosure() -> Any?,
     #endif
 }
 
+protocol OKDescriptable {
+    func propertyDescription() -> String
+}
+
+extension OKDescriptable {
+    func propertyDescription() -> String {
+        let strings = Mirror(reflecting: self).children.flatMap { "\($0.label!): \($0.value)" }
+        var string = ""
+        for str in strings {
+            string += str + "\n"
+        }
+        return string
+    }
+}
+
+
